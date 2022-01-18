3fa8ad68af2abed101aaefed4f7187234dda2aac
심규원 <kwsim@nicepni.co.kr>
2022-01-18 19:58:38 +0900

    크롤러 클래스 구현중, JSoup 버전 명시

--- src/main/java/com/alimi/kofia/domain/KofiaCrawler.java
+++ src/main/java/com/alimi/kofia/domain/KofiaCrawler.java
@@ -1,12 +1,21 @@
 package com.alimi.kofia.domain;
 
 import com.alimi.kofia.domain.posts.Posts;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class KofiaCrawler {
-    private final String sURL = "http://www.kofia.or.kr/brd/m_96/list.do";
+    private final String URL = "http://www.kofia.or.kr/brd/m_96/list.do";
+
+    private Document getHTML() throws IOException {
+        Connection conn = Jsoup.connect(this.URL);
+        return conn.get();
+    }
 
     public static List<Posts> getPostsList() {
         List<Posts> posts = new ArrayList<>();
